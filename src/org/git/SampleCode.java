f1e159b96a7d469be7fe97c69dced5545ceda6a7
showmiya13 <showmishowmiya23@gmail.com>
2023-03-02 12:17:50 +0530

    code added

---
+++ src/org/git/SampleCode.java
@@ -0,0 +1,9 @@
+package org.git;
+
+public class SampleCode {
+public static void main(String[] args) {
+	System.out.println("Enter your branch");
+	System.out.println("Enter your class");
+	System.out.println("Enter your code");
+}
+}
